7f96c6f0d96338e6deba223a8d771c2354a9ae57
anzhb <anzhalika.baradzina@rebelmouse.com>
2021-10-31 22:32:39 +0300

    task1 added

--- hw3/task1/task1.js
+++ hw3/task1/task1.js
@@ -1,31 +1,30 @@
 
+  var fs=require('fs');
+  var jsonData =fs.readFileSync('data.json', 'utf8');
+  var dataFromJsonToBeChanged = JSON.parse(jsonData);
+  let dataFromJson = JSON.parse(JSON.stringify(dataFromJsonToBeChanged));
 
-function getPricesFromJson () {
-    var fs=require('fs');
-    var jsonData =fs.readFileSync('data.json', 'utf8');
-    var dataFromJson = JSON.parse(jsonData);
-    
-    console.log(jsonData)
-    console.log(dataFromJson)
-    
-    console.log(dataFromJson.data[0].SKU)
-    
-    //console.log(dataFromJson.data[0].Prices.Price)
-    let course = 0.86
-    var values = []
-    var keys = []
-    for(var k in dataFromJson.data[0].Prices) {
-      values.push(dataFromJson.data[0].Prices[k]*course)
-    
-      keys.push(k)
-    
-    
-    }
-    
-    
-    console.log(values)
-    console.log(keys)
-    }
-    
-    getPricesFromJson ()
-    
+  dataFromJsonToBeChanged.data.forEach((el) => {
+    el.Prices.Currency = "USD";
+  });
+
+  let course = 0.86
+
+  dataFromJson.data.forEach((el) => {
+    el.Prices.Price = Math.round(
+      ((el.Prices.Price * course) * 100) / 100
+    );
+
+    el.Prices["Retail Price"] = Math.round(
+      ((el.Prices["Retail Price"] * course) * 100) / 100
+    );
+
+    el.Prices["Wholesale Price"] = Math.round(
+      ((el.Prices["Wholesale Price"] * course) * 100) / 100
+    );
+
+    el.Prices.Currency = "EUR";
+  });
+
+  fs.writeFileSync("dataBeforeUpdated.json", JSON.stringify(dataFromJsonToBeChanged));
+  fs.writeFileSync("updatedData.json", JSON.stringify(dataFromJson));
